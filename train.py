d69e88ef343731d1f92ec6c1726f8dacf38f5472
Marek Charvát <85548309+charviis@users.noreply.github.com>
2022-05-17 11:16:47 +0200

    AI ready

---
+++ train.py
@@ -0,0 +1,26 @@
+import cv2
+from matplotlib import pyplot as plt
+
+img = cv2.imread("image.jpg")
+
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+stop_data = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+
+found = stop_data.detectMultiScale(img_gray,
+								minSize =(20, 20))
+
+amount_found = len(found)
+
+if amount_found != 0:
+
+	for (x, y, width, height) in found:
+
+		cv2.rectangle(img_rgb, (x, y),
+					(x + height, y + width),
+					(0, 255, 0), 5)
+
+plt.subplot(1, 1, 1)
+plt.imshow(img_rgb)
+plt.show()
