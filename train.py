a59878e4b3a3455969db1f09c2485f84eaaf7b29
Marek Charvát <85548309+charviis@users.noreply.github.com>
2022-05-19 08:57:06 +0200

    change

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ img = cv2.imread("image.jpg")
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-stop_data = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+stop_data = cv2.CascadeClassifier('stop_data.xml')
 
 found = stop_data.detectMultiScale(img_gray,
 								minSize =(20, 20))
