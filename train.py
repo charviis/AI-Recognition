83776417acfa27b73ae97c2c801ffb66b2980fc5
Marek Charvát <marekgta233@gmail.com>
2022-06-05 15:50:10 +0200

    img ups

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import cv2
 from matplotlib import pyplot as plt
 
-img = cv2.imread("image3.jpg")
+img = cv2.imread("image.jpg")
 
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
