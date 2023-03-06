10f4c861218d23d77635d945aa22fbe319e2e505
Marek Charvát <85548309+charviis@users.noreply.github.com>
2023-03-06 23:56:33 +0100

    finished project with gui and documentation

---
+++ source/README.md
@@ -0,0 +1,11 @@
+# AI-Recognition
+
+<p>Rozpoznávání dopravních značek je schopnost stroje nebo počítačového systému identifikovat a interpretovat dopravní značky v reálném čase. Toto je pro autonomní vozidla klíčová funkce, neboť jim umožňuje se bezpečně a efektivně pohybovat po silnicích.</p>
+
+<p>Tradičně bylo rozpoznávání dopravních značek prováděno pomocí strojového učení, které bylo vycvičeno na velkém datasetu obrázků dopravních značek. Tyto algoritmy se naučí rozpoznávat vzory a rysy v obrázcích, které jsou charakteristické pro různé dopravní značky.</p>
+
+<p>Existuje několik přístupů k trénování těchto algoritmů, včetně supervizovaného učení, kde jsou algoritmy vycvičeny na označeném datasetu obrázků, a nesupervizovaného učení, kde algoritmy naučí rozpoznávat vzory v datech bez explicitních označení.</p>
+
+<p>Jednou z klíčových výzev při rozpoznávání dopravních značek je velká variabilita vzhledu dopravních značek. Dopravní značky se mohou lišit ve velikosti, tvaru, barvě a písmu a mohou být částečně zakryty nebo překryty jinými objekty. Aby bylo možné tyto výzvy zvládnout, moderní algoritmy pro rozpoznávání dopravních značek používají kombinaci technik hlubokého učení, jako jsou konvoluční neuronové sítě (CNN), a tradičních algoritmů strojového učení k dosahování vysoké přesnosti.</p>
+
+<p>Kromě toho, komponenty strojového učení obsahují systémy pro rozpoznávání dopravních značek obvykle také komponentu počítačového vidění, která je zodpovědná za detekci a sledování dopravních značek</p>
---
+++ work/GuiCode.py
@@ -0,0 +1,67 @@
+import os
+import tkinter as tk
+from tkinter import filedialog
+from PIL import ImageTk, Image
+import numpy as np
+import cv2
+import sys
+
+# import the Haarcascade classifier
+face_cascade = cv2.CascadeClassifier( 'work\stop_data.xml' )
+
+# initialize the GUI
+root = tk.Tk()
+root.title("Sign Detection")
+root.geometry("800x700")
+root.minsize(width=500, height=500)
+root.configure(background='dark grey')
+root.option_add("*Font", "Helvetica 20")
+root.option_add("*Button.Relief", "flat")
+root.option_add("*Button.Background", "grey")
+root.option_add("*Button.ActiveBackground", "white")
+root.option_add("*Button.Foreground", "black")
+root.option_add("*Button.ActiveForeground", "black")    
+root.option_add("*Button.BorderWidth", "0")
+root.resizable(False, False)
+
+# function to open an image file and display it on the GUI
+def open_image():
+    path = filedialog.askopenfilename()
+    image = cv2.imread(path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = Image.fromarray(image)
+    image = ImageTk.PhotoImage(image)
+    image_label.config(image=image)
+    image_label.image = image
+    detect_signs(path)
+
+# error handling
+if face_cascade.empty():
+    print("Error: Unable to load the classifier")
+    sys.exit(0)
+
+# function to detect the signs in the image
+def detect_signs(path):
+    image = cv2.imread(path)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    signs = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+    for (x,y,w,h) in signs:
+        cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = Image.fromarray(image)
+    image = ImageTk.PhotoImage(image)
+    image_label.config(image=image)
+    image_label.image = image
+    
+# button to open an image file
+open_button = tk.Button(root, text="Open Image", command=open_image)
+open_button.pack()
+
+# label to display the selected image
+image_label = tk.Label(root)
+image_label.pack()
+
+# create console log.txt file
+sys.stdout = open("work\log.txt", "w")
+
+root.mainloop()
